8b035acec9c1eb993eeff0fea02309b1b8d2dc0e
Ramil <soroka250402@yandex.ru>
2019-01-10 20:47:00 +0400

    Create templates folder and update style.css, index.html, index.js and img folder
    Background and filter settings sections are added, but without script, design and adaptivity.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,54 @@
-c = $('#bg-picture__content')[0];
-ctx = c.getContext('2d');
-img = new Image();
-img.src = '../img/index/bg_preview/bg.png';
-img.addEventListener('load', () => {
-	ctx.drawImage(img, 0, 0);
+/*
+c - canvas
+ctx - canvas's context
+img - image for canvas
+
+fbl - first background layer
+sbl - second background layer
+tbl - third background layer
+fntl - font layer
+*/
+
+bg_name = '0'
+font_name = '0'
+filter_name = '0'
+
+fbl_c = $('#first-bg-layer')[0];
+fbl_ctx = fbl_c.getContext('2d');
+fbl_img = new Image();
+fbl_img.src = '../templates/backgrounds/' + bg_name + '/first_layer.png';
+fbl_img.addEventListener('load', () => {
+	fbl_ctx.drawImage(fbl_img, 0, 0);
+});
+
+sbl_c = $('#first-bg-layer')[0];
+sbl_ctx = sbl_c.getContext('2d');
+sbl_img = new Image();
+sbl_img.src = '../templates/backgrounds/' + bg_name + '/second_layer.png';
+sbl_img.addEventListener('load', () => {
+	sbl_ctx.drawImage(sbl_img, 0, 0);
+});
+
+tbl_c = $('#first-bg-layer')[0];
+tbl_ctx = tbl_c.getContext('2d');
+tbl_img = new Image();
+tbl_img.src = '../templates/backgrounds/' + bg_name + '/third_layer.png';
+tbl_img.addEventListener('load', () => {
+	tbl_ctx.drawImage(tbl_img, 0, 0);
+});
+
+fntl_c = $('#font-layer')[0];
+fntl_ctx = fntl_c.getContext('2d');
+fntl_img = new Image();
+fntl_img.src = '../templates/fonts/' + font_name + '/preview.png';
+fntl_img.addEventListener('load', () => {
+	fntl_ctx.drawImage(fntl_img, 0, 0);
+});
+
+fltrl_c = $('#filter-layer')[0];
+fltrl_ctx = fltrl_c.getContext('2d');
+fltrl_img = new Image();
+fltrl_img.src = '../templates/filters/' + filter_name + '/filter.png';
+fltrl_img.addEventListener('load', () => {
+	fltrl_ctx.drawImage(fltrl_img, 0, 0);
 });
